ed4874e490aa3f86ff16cd1a78671370ab9908e8
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-05-07 06:13:13 +0600

    responsiveness complete

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -20,11 +20,11 @@ const MyProfile = () => {
       <Helmet>
         <title>{pageTitle}</title>
       </Helmet>
-      <div className='w-11/12 mx-auto flex flex-col md:flex-row py-8'>
-      <div>
-        <img src="https://i.ibb.co.com/2YpXbGD1/profile.gif" alt="gif" />
+      <div className='w-11/12 mb-12 gap-8 justify-center items-center px-2 mx-auto flex flex-col lg:flex-row py-8'>
+      <div className='w-full'>
+        <img  src="https://i.ibb.co.com/2YpXbGD1/profile.gif" alt="gif" />
       </div>
-      <div className="flex flex-col justify-center items-center mx-auto py-10 px-5 shadow-lg rounded-xl  bg-base-200 text-gray-800 space-y-4">
+      <div className="flex flex-col justify-center items-center mx-auto md:px-5 py-10 px-8 shadow-lg rounded-xl  bg-base-200 text-gray-800 space-y-4">
         <div className='avatar'>
           <div className='w-34 rounded-full'>
             <img src={user.photoURL
@@ -33,14 +33,14 @@ const MyProfile = () => {
         </div>
         <div className="space-y-4 text-center divide-y dark:divide-gray-300">
           <div className="my-2 space-y-1">
-            <h2 className="text-xl font-medium text-gray-500"> <span className=' font-bold'>Name : </span>{user.
+            <h2 className="text-lg font-medium text-gray-500"> <span className=' font-bold'>Name : </span>{user.
               displayName
             } </h2>
-            <h2 className="text-md font-medium text-gray-500"><span className=' font-bold '>Email : </span> {user.email
+            <h2 className="text-sm font-medium text-gray-500"><span className=' font-bold '>Email : </span> {user.email
             } </h2>
-            <h2 className="text-md font-medium text-gray-500"> <span className='font-bold'>PhotoURL: </span>{user.photoURL
+            <h2 className="text-sm hidden md:block font-medium text-gray-500"> <span className='font-bold'>PhotoURL: </span>{user.photoURL
             } </h2>
-            <h2 className="text-md font-medium text-gray-500"> <span className='font-bold'>UID : </span>{user.uid
+            <h2 className="text-sm font-medium text-gray-500"> <span className='font-bold'>UID : </span>{user.uid
             } </h2>
 
           </div>
